d42979062dc53c108d0864bdb831aa5ed9685e27
Gu Chenglin <1470688049@qq.com>
2026-09-17 09:25:29 +0800

    feat: add main branch message

--- main.c
+++ main.c
@@ -4,5 +4,5 @@ int main()
 {
     // @TODO: print a sentence you want.
     printf("Hello, world!\n");
-    printf("24300680080: my first commit in Lab0.\n");
+    printf("24300680080: this line is written in the main branch.\n");
 }
