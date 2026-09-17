15938928110ce1fd9088e56049057dcae54b3951
Gu Chenglin <1470688049@qq.com>
2026-09-17 10:14:25 +0800

    fix: remove leftover conflict markers in main.c

--- main.c
+++ main.c
@@ -4,9 +4,6 @@ int main()
 {
     // @TODO: print a sentence you want.
     printf("Hello, world!\n");
-<<<<<<< HEAD
     printf("24300680080: this line is written in the main branch.\n");
-=======
-    printf("24300680080:  this line is written in the feature branch.\n");
->>>>>>> feature
+    printf("24300680080: this line is written in the feature branch.\n");
 }
